10e3d7cd261ca20dfa21d40f05cf8b9d33fa959c
jcherrera <jeancarlosherreram@gmail.com>
2024-02-04 13:04:46 -0500

    Adding project files

---
+++ Training/Calculator.cs
@@ -0,0 +1,16 @@
+namespace Training;
+
+public class Calculator
+{
+    private readonly int _firstDigit, _secondDigit;
+    public Calculator(int firstDigit, int secondDigit)
+    {
+        _firstDigit = firstDigit;
+        _secondDigit = secondDigit;
+    }
+
+    public int Sum ()
+    {
+        return _firstDigit + _secondDigit;
+    }
+}
---
+++ Training/Program.cs
@@ -0,0 +1,7 @@
+﻿using Training;
+
+Calculator calculator = new(25, 4);
+
+var sumResult = calculator.Sum();
+
+Console.WriteLine($"The result is: {sumResult}");
